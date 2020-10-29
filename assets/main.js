812c58925d879d508117640b3d33b0f971734194
FrancescoDale <fd060862@gmail.com>
2020-10-29 16:51:01 +0100

    genero 5 numeri casuali

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
 
     var randSeq = []; //array con i 5 numeri generati casualmente
 
+    for (var i = 5; i > 0; i--) {
+        var randNum = Math.floor((Math.random() * 100)+1);
+        randSeq.push(randNum);
+        console.log(randSeq);
+    }
+
+
 
 
 }
