d9bef870407926eb9a94e25c48c24cd92589864e
FrancescoDale <fd060862@gmail.com>
2020-10-29 16:59:30 +0100

    dopo aver generato i numeri, li visualizzo nell'html creando altri span  con append

--- assets/main.js
+++ assets/main.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
 
     var randSeq = []; //array con i 5 numeri generati casualmente
 
+    // ciclo che genera 5 numeri casuali e li mette in un array
+
     for (var i = 5; i > 0; i--) {
         var randNum = Math.floor((Math.random() * 100)+1);
         randSeq.push(randNum);
         console.log(randSeq);
     }
 
+    // visualizza nell'html i numeri che sono stati generati
 
+    $( ".numb_list span" ).each(function() {
+       $(this).append(randSeq);
+   });
 
 
 }
